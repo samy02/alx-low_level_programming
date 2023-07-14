43d9bb5403bb252f235d92c2233adcdcae53fcc3
samy02 <samy.wagyh4@gmail.com>
2023-07-14 19:34:51 +0000

    adding command for the secound task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,9 @@
 #include <stdio.h>
 
 /**
- * this is some sort of comment
- *
- *
- * and here is the code
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
