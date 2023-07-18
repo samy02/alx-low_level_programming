54656862814d1cb8436b2e4cee92c831a3c05d9c
samy02 <samy.wagyh4@gmail.com>
2023-07-18 14:49:31 +0000

    fixing 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /*
- * main - this returns the word _putchar
- * Return: 0 (sucsses)
+ * main -  returns the word _putchar followed by a new line
+ * Return: Always 0 (sucsses)
  */
 int main(void)
 {
