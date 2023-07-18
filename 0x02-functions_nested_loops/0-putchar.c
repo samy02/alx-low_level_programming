ab77965bb16a74b181d0fc4f7d9c049da54cec9b
samy02 <samy.wagyh4@gmail.com>
2023-07-18 13:11:01 +0000

    adding loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ *main - this returns the word _putchar
+ * Return: 0 (sucsses)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
