85b57e6961961629d3eef64e772762e7f224b08b
samy02 <samy.wagyh4@gmail.com>
2023-07-18 15:53:23 +0000

    fixing 7

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 	char j;
 	for(i=0; i<=10; i++)
 	{
-		for(j=a; j<=z; j++)
+		for(j=97; j<=122; j++)
 		{
-			_putchar('char j');
+			_putchar('int j');
 		}
 		_putchar('\n');
 	}
