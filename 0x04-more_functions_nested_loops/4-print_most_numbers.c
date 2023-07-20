a8c8654fdbb06e2b80b5d7be47ce8a8a21db5a33
samy02 <samy.wagyh4@gmail.com>
2023-07-20 17:30:00 +0000

    fixing 4 and 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints the numbers 0 to 9 except 2 and 4 followed by a new line
+ * print_most_numbers - prints the numbers 0 to 9 
+ * except 2 and 4 followed by a new line
  */
 void print_most_numbers(void)
 {
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the numbers 1 to 100, followed by a new line but for multiples of three prints Fizz instead of the number and for the multiples of five prints Buzz
+ * main - prints the numbers 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
 int main(void)
