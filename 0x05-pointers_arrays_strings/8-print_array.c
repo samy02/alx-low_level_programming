2e78c62212138781011a72ed5d4934e9c31ecb6c
samy02 <samy.wagyh4@gmail.com>
2023-07-24 14:40:26 +0000

    fixing betty

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_array - prints n elements of an array of integers followed by a new line
+ * print_array - prints n elements of an array of integers and a new line
  * @a: array to be printed
  * @n: number of elements to print
  */
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * *_strcpy - copies the string pointed to by src including the terminating null byte (\0)
- * to the buffer pointed to by dest
+ * *_strcpy - copies the string pointed to by src
+ * including the terminating null byte (\0) to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
  * Return: the pointer to dest
