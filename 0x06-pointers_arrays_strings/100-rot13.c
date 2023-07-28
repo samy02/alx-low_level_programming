6a8eccfff6f8be6b09695437d823a4be79757a96
samy02 <samy.wagyh4@gmail.com>
2023-07-28 20:01:55 +0000

    fixing 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,7 +3,6 @@
 /**
  * rot13 - encodes a string in rot13
  * @s: string to be encoded
- *
  * Return: the resulting string
  */
 char *rot13(char *s)
@@ -11,7 +10,7 @@ char *rot13(char *s)
 	int i, j;
 
 	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char b[] = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
